634446d87f88d4444200025f425a3527b86f4e59
Elad Raveh <eladraveh@nova.cs.tau.ac.il>
2023-05-01 14:35:06 +0300

    after_handling_k

---
+++ Sanity_Check/spkmeans.c
@@ -0,0 +1,271 @@
+# include "spkmeans.h"
+
+int LINE_SIZE = 10;
+int COLS = 0;
+int ROWS = 0;
+
+/***************************
+ * File Processing Functions
+ ***************************/
+
+/*
+Func processes the points file.
+param path: the path of the points file.
+return: the processed points matrix.
+*/
+double ** processFile(char *path) {
+    double *point, **points;
+    FILE *file = fopen(path, "r");
+    if (file == NULL) {return NULL;}
+    point = processFirstLine(file);
+    if (point == NULL) {
+        fclose(file);
+        return NULL;
+    }
+    points = createMat(10, COLS);
+    if (points == NULL) {
+        free(point);
+        fclose(file);
+        return NULL;
+    }
+    copyDoublePFromTo(point, points[0], COLS);
+    points = processPoints(points, file);
+    free(point);
+    fclose(file);
+    return points;
+}
+
+/*
+Func processes the first line of the file to COLS and suspected LINE_SIZE.
+param file: the pointer to the file object.
+return: double *point - the first point in the file.
+*/
+double * processFirstLine(FILE *file) {
+    char *line;
+    int *indp, size = 2;
+    double *point = (double *) malloc((size_t) (size * sizeof(double)));
+    if (point == NULL) {return NULL;}
+    line = getLine(file);
+    if (line == NULL) {
+        free(point);
+        return NULL;
+    }
+    indp = (int *) malloc(sizeof(int));
+    if (indp == NULL) {
+        free(point);
+        free(line);
+        return NULL;
+    }
+    *indp = 0;
+    while (line[(*indp)] != '\0') {
+        point[COLS++] = processNum(line, indp);
+        if (MEM_ERR) {return NULL;}
+        if (size == COLS) {
+            point = (double *) realloc(point, (size_t) ((size *= 2) * sizeof(double)));
+            if (point == NULL) {
+                free(indp);
+                free(line);
+                return NULL;
+            }
+        }
+        (*indp)++;
+    }
+    free(indp);
+    free(line);
+    return point;
+}
+
+/*
+Func recieves a single line from the given file.
+param file: the pointer to the file object.
+return: given char * of line.
+*/
+char * getLine(FILE *file) {
+    char *pointer, *line;
+    int *indp = (int *) malloc(sizeof(int));
+    if (indp == NULL) {return NULL;}
+    *indp = 0;
+    line = (char *) malloc((size_t) (LINE_SIZE * sizeof(char)));
+    if (line == NULL) {
+        free(indp);
+        MEM_ERR = 1;
+        return NULL;
+    }
+    pointer = fgets(line, LINE_SIZE, file);
+    if (pointer == NULL) {
+        free(line);
+        free(indp);
+        return NULL;
+    }
+    while (!checkGotLine(line, indp)) {
+        line = (char *) realloc(line, (size_t) ((LINE_SIZE * 2) * sizeof(char)));
+        if (line == NULL) {
+            free(pointer);
+            free(indp);
+            MEM_ERR = 1;
+            return NULL;
+        }
+        enterAfter(line + *indp, file);
+        LINE_SIZE *= 2;
+    }
+    free(indp);
+    return line;
+}
+
+/*
+Func checks for the success of the memory allocatin needed to recieve a line.
+param line: points to the recieved line - saved as a char array.
+param indp: a pointer to the last checked index of the array.
+return: 1 on success, 0 on failure.
+*/
+int checkGotLine(char *line, int *indp) {
+    for (; *indp < LINE_SIZE; (*indp)++) {if (line[*indp] == '\0') {return line[*indp - 1] == '\n';}}
+    return 0;
+}
+
+/*
+Func adds the rest of the line to the information recieved if the line size was too small.
+param line: points to the end of the recieved line.
+param file: the pointer to the file object.
+*/
+void enterAfter(char *line, FILE *file) {line = fgets(line, LINE_SIZE, file);}
+
+/*
+Func computes the numerical value of a double given as a string.
+param line: the line from which we try to read the number.
+param indp: the index of the first char which is a part of the number.
+return: the numerical value of a double given as a string.
+*/
+double processNum(char *line, int *indp) {
+    int size = 8, i = 0, start = *indp;
+    double num;
+    char *numStr = (char *) malloc((size_t) (size * sizeof(char)));
+    if (numStr == NULL) {
+        MEM_ERR = 1;
+        return 0.0;
+    }
+    while (line[*indp] != ',' && line[*indp] != '\n') {
+        numStr[i] = line[(*indp)++];
+        if (size == ++i) {
+            numStr = (char *) realloc(numStr, (size_t) ((size *= 2) * sizeof(char)));
+            if (numStr == NULL) {
+                MEM_ERR = 1;
+                return 0.0;
+            }
+        }
+    }
+    numStr = (char *) realloc(numStr, (size_t) ((*indp - start + 1) * sizeof(char)));
+    if (numStr == NULL) {
+        MEM_ERR = 1;
+        return 0.0;
+    }
+    numStr[*indp - start] = '\0';
+    num = atof(numStr);
+    free(numStr);
+    return num;
+}
+
+/*
+Func processes the points given in the file to the given pointers.
+param clusters: a pointer to the (empty) clusters matrix.
+param nums_entered: an integer array signifyign how many numbers went into each centroid.
+param file: the pointer to the file object.
+return: double ** matrix containing all of the points in the file.
+*/
+double ** processPoints(double **points, FILE *file) {
+    int length = 10;
+    double *curPoint;
+    while ((curPoint = processLine(file)) != NULL) {
+        if (ROWS++ == length - 1) {
+            points = doubleMatLength(points, length, COLS);
+            if (points == NULL) {
+                free(curPoint);
+                return NULL;
+            }
+            length *= 2;
+        }
+        copyDoublePFromTo(curPoint, points[ROWS], COLS);
+        free(curPoint);
+    }
+    ROWS++;
+    if (MEM_ERR) {return NULL;}
+    return points;
+}
+
+/*
+Func processes a line of input from the given file.
+param file: the pointer to the file object.
+return: double * point - a numerical represntation of the line.
+*/
+double * processLine(FILE *file) {
+    double *point;
+    int *indp, i = 0;
+    char *line = getLine(file);
+    if (line == NULL) {return NULL;}
+    point = (double *) malloc((size_t) (COLS * sizeof(double)));
+    if (point == NULL) {
+        free(line);
+        MEM_ERR = 1;
+        return NULL;
+    }
+    indp = (int *) malloc(sizeof(int));
+    if (indp == NULL) {
+        free(line);
+        free(point);
+        MEM_ERR = 1;
+        return NULL;
+    }
+    *indp = 0;
+    for (; i < COLS; i++) {
+        point[i] = processNum(line, indp);
+        (*indp)++;
+    }
+    free(line);
+    free(indp);
+    return point;
+}
+
+int main(int argc, char *argv[]) {
+    double **points, **mat, **helpMat;
+    if (argc != 3) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+    if (strcmp(argv[1], "jacobi") && strcmp(argv[1], "wam") && strcmp(argv[1], "ddg") && strcmp(argv[1], "gl")) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+    points = processFile(argv[2]);
+    if (points == NULL) {
+        printf("An Error Has Occurred\n");
+        return 1;
+    }
+    if (!strcmp(argv[1], "jacobi")) {
+        mat = itterRots(points, ROWS);
+        if (mat == NULL) {
+            printf("An Error Has Occurred\n");
+            return 1;
+        }
+    }
+    else {
+        mat = calcWeightedAdjencyMatrix(points, ROWS, COLS);
+        if (mat == NULL) {
+            printf("An Error Has Occurred\n");
+            return 1;
+        }
+        if (strcmp(argv[1], "wam")) {
+            helpMat = calcDiagonalDegreeMatrix(mat, ROWS);
+            if (helpMat == NULL) {
+                printf("An Error Has Occurred\n");
+                return 1;
+            }
+            if (!strcmp(argv[1], "gl")) {calcLaplasianMatrix(mat, helpMat, ROWS);}
+            freeMat(mat);
+            mat = helpMat;
+        }
+    }
+    printMat(mat, ROWS + (strcmp(argv[1], "jacobi") ? 0:1), ROWS);
+    freeMat(points);
+    freeMat(mat);
+    return 0;
+}
---
+++ Sanity_Check/spkmeans.h
@@ -0,0 +1,502 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <math.h>
+# include <string.h>
+
+int MEM_ERR = 0;
+static int MAX_R_ITTER = 100;
+static double EPS_R = 0.00001;
+
+/*General Functions*/
+int * createDupArray(int, int);
+void copyDoublePFromTo(double *, double *, int);
+
+/*Matrix Handling Functions*/
+double ** createMat(int, int);
+void copyMatFromTo(double **, double **, int, int);
+double ** doubleMatLength(double **, int, int);
+void zeroMat (double **, int, int);
+void divideMat(double **, int, int, int *);
+double ** transposeMat(double **, int, int);
+double ** sortMat(double **, int, int);
+double ** idMat(int);
+void subtractMatrix(double **, double **, int);
+double ** mulSqMats(double **, double **, int);
+void printMat(double **, int, int);
+void freeMat(double **);
+
+/*File Processing Functions*/
+double ** processFile(char *);
+double * processFirstLine(FILE *);
+char * getLine(FILE *);
+int checkGotLine(char *, int *);
+void enterAfter(char *, FILE *);
+double processNum(char *, int *);
+double ** processPoints(double **, FILE *);
+double * processLine(FILE *);
+
+/*Jacobi Functions*/
+double ** itterRots(double **, int);
+int * maxAbsVal(double **, int);
+double ** createRotMat(double **, int, int, int);
+int didRotConverge(double **, double **, int);
+double calcOff(double **, int);
+double ** buildJacobiRet(double **, double **, int);
+int eigenHur(double *, int);
+
+/*wam, ddg, gl Functions*/
+double squaredEuclideanDistance(double *, double *, int);
+double ** calcWeightedAdjencyMatrix(double **, int ,int);
+double findTheDegreeOfaVertex(double *, int);
+double ** calcDiagonalDegreeMatrix(double **, int);
+void calcLaplasianMatrix(double **, double **, int);
+
+/*kmeans Functions*/
+int itterPoints(double **, double **);
+int closestCluster(double **, double *);
+double distance(double *, double *, int);
+void addPoint(double *, double *);
+int didPointsConverge(double **, double **, int, int);
+
+/*******************
+ * General Functions
+ *******************/
+
+/*
+Func creates an integer pointer (array) of a given length and value for all cells.
+param length: the amount of cells rquired.
+param num: the numbr which is to be repaeated in every cell.
+return: an int * pointer of size length in which each cell contains num.
+*/
+int * createDupArray(int length, int num) {
+    int i = 0, *dupArray = (int *) malloc((size_t) (length * sizeof(int)));
+    if (dupArray == NULL) {return NULL;}
+    for (; i < length; i++) {dupArray[i] = num;}
+    return dupArray;
+}
+
+/*
+Func copies a double * pointer into another double *  until a given index.
+param from: the array storing the copied information.
+param to: the array which the information will be copied to.
+param length: the size of the arrays.
+*/
+void copyDoublePFromTo(double *from, double *to, int length) {
+    int i = 0;
+    for (; i < length; i++) {to[i] = from[i];}
+}
+
+/***************************
+ * Matrix Handling Functions
+ ***************************/
+
+/*
+Func creates double ** pointer to a contigous block (matrix) of memory.
+param rows: the ammount of required rows.
+param cols: the amount of required cols.
+return: the double ** pointer to the matrix.
+*/
+double ** createMat(int rows, int cols) {
+    double *mat, **mat_loc;
+    int i = 0;
+    mat = (double *) malloc((size_t) (rows * cols * sizeof(double)));
+    if (mat == NULL) {return NULL;}
+    mat_loc = (double **) malloc((size_t) (rows * sizeof(double *)));
+    if (mat_loc == NULL) {
+        free(mat);
+        return NULL;
+    }
+    for (; i < rows; i++) {mat_loc[i] = mat + i * cols;}
+    return mat_loc;
+}
+
+/*
+Func copies a double ** matrix (contigous block) into another double ** matrix.
+param from: the matrix storing the copied information.
+param to: the matrix which the information will be copied to.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+*/
+void copyMatFromTo(double **from, double **to, int rows, int cols) {
+    int i = 0;
+    for (; i < rows; i++) {copyDoublePFromTo(from[i], to[i], cols);}
+}
+
+/*
+Func doubles the length of a given matrix while preserving the infomratin.
+param points: the given matrix.
+param length: the amount of rows in the matrix.
+return: A pointer to the doubled length matrix.
+*/
+double ** doubleMatLength(double **points, int rows, int cols) {
+    double **tmp = createMat(rows * 2, cols);
+    if (tmp == NULL) {return NULL;}
+    copyMatFromTo(points, tmp, rows, cols);
+    freeMat(points);
+    return tmp;
+}
+
+/*
+Func sets all the values of a matrix to zero.
+param matrix: the matrix we would like to zero.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+*/
+void zeroMat(double **mat, int rows, int cols) {
+    int i = 0, j;
+    for (; i < rows; i++) {for (j = 0; j < cols; j++) {mat[i][j] = 0;}}
+}
+
+/*
+Func divides each element in a matrix according to the value matching it's row.
+param matrix: the matrix we would like to divide.
+param divider: an array of ints signifying which row should be divided by which number.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+*/
+void divideMat(double **matrix, int rows, int cols, int *divider) {
+    int i = 0, j;
+    for (; i < rows; i++) {for (j = 0; j < cols; j++) {matrix[i][j] /= divider[i];}}
+}
+
+/*
+Func transposes a matrix.
+param mat: the matrix we would like to zero.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+return: the transposed matrix.
+*/
+double ** transposeMat(double **mat, int rows, int cols) {
+    int i = 0, j;
+    double **tposed = createMat(cols, rows);
+    if (tposed == NULL) {return NULL;}
+    for (; i < rows; i++) {for (j = 0; j < cols; j++) {tposed[j][i] = mat[i][j];}}
+    return tposed;
+}
+
+/*
+Func sorts a matrix using it's first row.
+param mat: the matrix we would like to sort.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+return: the sorted matrix.
+*/
+double ** sortMat(double **mat, int rows, int cols) {
+    int i, j;
+    double *tmp, **sorted, **toSort = transposeMat(mat, rows, cols);
+    tmp = (double *) malloc((size_t) (rows * sizeof(double)));
+    if (toSort == NULL) {return NULL;}
+    for (i = 1; i < cols; i++) {
+        copyDoublePFromTo(toSort[i], tmp, rows);
+        j = i;
+        while (j > 0 && toSort[j - 1][0] > tmp[0]) {
+            copyDoublePFromTo(toSort[j - 1], toSort[j], rows);
+            j -= 1;
+        }
+        copyDoublePFromTo(tmp, toSort[j], rows);
+    }
+    sorted = transposeMat(toSort, cols, rows);
+    freeMat(toSort);
+    free(tmp);
+    return sorted;
+}
+
+/*Func creates and returns an identity matrix of deimension dim*/
+double ** idMat(int dim) {
+    double **mat = createMat(dim, dim);
+    if (mat == NULL) {return NULL;}
+    zeroMat(mat, dim, dim);
+    for (; dim > 0; dim--) {mat[dim - 1][dim - 1] = 1;}
+    return mat;
+}
+
+/*subtracts mat2 from mat1 (the result is in mat1, both of the matrices are of the same dimension [n]).*/
+void subtractMatrix(double ** mat1, double ** mat2, int n) {
+    int i = 0, j;
+    for (; i < n; i++) {for (j = 0;  j < n; j++) {mat1[i][j] = mat1[i][j] - mat2[i][j];}}
+}
+
+/*
+Func implements matrix multiplication for two square matrices - A x B = P.
+param mat1: the left matrix in the multiplication process (A).
+param mat2: the right matrix in the multiplication process (B).
+param dim: The number of rows and columns in the matrices.
+return: The matrix P as listed above.
+*/
+double ** mulSqMats(double **mat1, double **mat2, int dim) {
+    int j, cur, i = 0;
+    double **prod = createMat(dim, dim);
+    if (prod == NULL) {return NULL;}
+    zeroMat(prod, dim, dim);
+    for (; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            for (cur = 0; cur < dim; cur++) {prod[i][j] += mat1[i][cur] * mat2[cur][j];}
+        }
+    }
+    return prod;
+}
+
+/*
+Func prints a given matrix.
+param mat: the matrix we would like to print.
+param rows: the amount of rows in the matrix.
+param cols: the amount of columns in the matrix.
+*/
+void printMat(double **mat, int rows, int cols) {
+    int i = 0, j;
+    if (mat == NULL) {printf("mat: An error has accured\n");}
+    else {
+        for (; i < rows; i++) {
+            for (j = 0; j < cols - 1; j++) {printf("%.4f,", mat[i][j]);}
+            printf("%.4f", mat[i][cols - 1]);
+            printf("\n");
+        }
+    }
+}
+
+/*Func frees a matrix created using createMat*/
+void freeMat(double **mat) {
+    free(mat[0]);    
+    free(mat);
+}
+
+/******************
+ * Jacobi Functions 
+ ******************/
+
+/*
+Func rotates a given symetrical matrix until it's diagonal/converged/max iteration has passed.
+param mat: a symetrical matrix to be diagonalized.
+param dim: the dimension of the matrix.
+return: The first line is the eigen values, from there on it's the product of the rotation matrices.
+*/
+double ** itterRots(double **mat, int dim) {
+    int *maxDim;
+    double **rotated, **halfRot, **curRot, **tRot, **prev, **curProd, **product = idMat(dim);
+    if (product == NULL) {return NULL;}
+    prev = createMat(dim, dim);
+    if (prev == NULL) {
+        freeMat(product);
+        return NULL;
+    }
+    rotated = createMat(dim, dim);
+    if (rotated == NULL) {
+        freeMat(product);
+        freeMat(prev);
+        return NULL;
+    }
+    copyMatFromTo(mat, rotated, dim, dim);
+    do {
+        copyMatFromTo(rotated, prev, dim, dim);
+        freeMat(rotated);
+        maxDim = maxAbsVal(prev, dim);
+        if (maxDim == NULL) {
+            freeMat(prev);
+            freeMat(product);
+            return NULL;
+        }
+        if (mat[maxDim[0]][maxDim[1]] == 0) {break;}
+        curRot = createRotMat(prev, dim, maxDim[0], maxDim[1]);
+        free(maxDim);
+        if (curRot == NULL) {
+            freeMat(prev);
+            freeMat(product);
+            return NULL;
+        }
+        tRot = transposeMat(curRot, dim, dim);
+        if (tRot == NULL) {
+            freeMat(curRot);
+            freeMat(prev);
+            freeMat(product);
+            return NULL;
+        }
+        halfRot = mulSqMats(tRot, prev, dim);
+        freeMat(tRot);
+        if (halfRot == NULL) {
+            freeMat(curRot);
+            freeMat(prev);
+            freeMat(product);
+            return NULL;
+        }
+        rotated = mulSqMats(halfRot, curRot, dim);
+        freeMat(halfRot);
+        if (rotated == NULL) {
+            freeMat(curRot);
+            freeMat(prev);
+            freeMat(product);
+            return NULL;
+        }
+        curProd = mulSqMats(product, curRot, dim);
+        freeMat(curRot);
+        if (curProd == NULL) {
+            freeMat(prev);
+            freeMat(rotated);
+            freeMat(product);
+            return NULL;
+        }
+        copyMatFromTo(curProd, product, dim, dim);
+        freeMat(curProd);
+    } while (--MAX_R_ITTER > 0 && !didRotConverge(prev, rotated, dim));
+    freeMat(prev);
+    prev = buildJacobiRet(rotated, product, dim);
+    freeMat(rotated);
+    freeMat(product);
+    return prev;
+}
+
+/*
+Func funds the location of the off axis element with the largest absolute value.
+param mat: the matrix we want to find the element from.
+param dim: the dimension of the matrix.
+return: the row and column of the element.
+*/
+int * maxAbsVal(double **mat, int dim) {
+    int j, i = 0;
+    int *maxDim = (int *) calloc((size_t) 2, sizeof(int));
+    maxDim[1] = 1;
+    if (maxDim == NULL) {return NULL;}
+    for (; i < dim; i++) {
+        for (j = 0; j < dim; j++) {
+            if ((i != j) && (fabs(mat[i][j]) >= fabs(mat[maxDim[0]][maxDim[1]]))) {
+                maxDim[0] = i;
+                maxDim[1] = j;
+            }
+        }
+    }
+    return maxDim;
+}
+
+/*
+Func creates a rotation matrix for the jacobi algorithm.
+param mat: the matrix we want to rotate.
+param dim: the dimension of the matrix.
+param maxRow: the row of the off axis element with the largest absolute value.
+param maxCol: the column of the off axis element with the largest absolute value.
+return: the rotation matrix.
+*/
+double ** createRotMat(double **mat, int dim, int maxRow, int maxCol) {
+    double **rot, t, c, s, theta = (mat[maxCol][maxCol] - mat[maxRow][maxRow]) / (2 * mat[maxRow][maxCol]);
+    t = (theta >= 0 ? 1:-1) / (fabs(theta) + sqrt(pow(theta, 2) + 1));
+    c = 1 / (sqrt(1 + pow(t, 2)));
+    s = t * c;
+    rot = idMat(dim);
+    if (rot == NULL) {return NULL;}
+    rot[maxRow][maxRow] = rot[maxCol][maxCol] = c;
+    rot[maxRow][maxCol] = s;
+    rot[maxCol][maxRow] = -s;
+    return rot;
+}
+
+/*
+Func checks for the convergence of the rotation process.
+param org: the matrix before the rotation.
+param new: the matrix after the rotation.
+param dim: the dimension of the matrices.
+return: 1 if converged, 0 otherwise.
+*/
+int didRotConverge(double **org, double **new, int dim) {return calcOff(org, dim) - calcOff(new, dim) <= EPS_R;}
+
+/*
+Func claculates the off parameter for convergence checking.
+param mat: the matrix we want to calculate the off of.
+param dim: the dimension of the matrix.
+return: the off value.
+*/
+double calcOff(double **mat, int dim) {
+    int i = 0, j;
+    double off = 0;
+    for (; i < dim; i++) {for (j = 0; j < dim; j++) {if (i != j) {off += pow(mat[i][j], 2);}}}
+    return off;
+}
+
+/*
+Func builds the matrix the jacobi algorithm returns from the given values.
+param vals: the matrix after the diagonlization process.
+param vectors: the product of the rotation matrices.
+param dim: the dimension of the matrices.
+return: the jacobi algorithm's wanted return value.
+*/
+double ** buildJacobiRet(double **vals, double **vectors, int dim) {
+    int j, i = 0;
+    double **jacobi = createMat(dim + 1, dim);
+    char *str = (char *) calloc((size_t) 50, sizeof(char));
+    if (jacobi == NULL) {return NULL;}
+    copyMatFromTo(vectors, jacobi + 1, dim ,dim);
+    for (; i < dim; i++) {
+        jacobi[0][i] = vals[i][i];
+        sprintf(str, "%.4f", jacobi[0][i]);
+        if (!strcmp(str, "-0.0000")) {for (j = 0; j < dim + 1; j++) {jacobi[i][j] *= -1;}}
+    }
+    free(str);
+    return jacobi;
+}
+
+/*
+Func finds the largest gap between 2 eigen values (numbers in the first line) and returns it's index + 1.
+param row: the first row of the matrix (the row in which we want to find the lragest gap).
+param length: the length of the row.
+return: the index of the gap + 1.
+*/
+int eigenHur(double *row, int length) {
+    double curGap, maxGap = 0;
+    int maxInd = 0, i = 0;
+    for (; i < (length - (length % 2 == 0 ? 0:1)) / 2; i++) {
+        curGap = fabs(row[i] - row[i + 1]);
+        if (maxGap < curGap) {
+            maxInd = i;
+            maxGap = curGap;
+        }
+    }
+    return maxInd + 1;
+}
+
+/************************
+ * wam, ddg, gl Functions
+ ************************/
+
+/* gets two points (as lists of doubles - p1, p2), same dimension (d). returns their squared euclidean distance.*/
+double squaredEuclideanDistance(double *p1, double *p2, int d) {
+    int i = 0;
+    double result = 0.0;
+    for (; i < d; i++) {result += pow((p1[i] - p2[i]), 2);}
+    return result;
+}
+
+/*gets a list of data points (dataPoints), the list length (n), and their dimension (d). clacs their weighted and returns it.*/
+double ** calcWeightedAdjencyMatrix(double ** dataPoints, int n ,int d) {
+    int i = 0, j;
+    double ** weightedMatrix = createMat(n, n);
+    if (weightedMatrix == NULL) {return NULL;}
+    for (; i < n; i++) {
+        for (j = 0; j < n; j++){
+            if (i == j) {weightedMatrix[i][j] = 0.0;}
+            else {
+                weightedMatrix[i][j] = exp(-0.5 * (squaredEuclideanDistance(dataPoints[i], dataPoints[j], d)));
+            }
+        }
+    }
+    return weightedMatrix;
+}
+
+/*gets the vertex's row from the weighted adjency matrix (vertexRow), and the number of data points (n). returns the vertex's degree as it define. */
+double findTheDegreeOfaVertex(double * vertexRow, int n) {
+    int i = 0;
+    double degree = 0.0;
+    for (; i < n; i++) {degree += vertexRow[i];}
+    return degree;
+}
+
+/*gets the weighted Adjency Matrix and the matrix dimension (n), returns te diagonal degree matrix as it describe.*/
+double ** calcDiagonalDegreeMatrix(double ** weightedAdjencyMatrix, int n) {
+    int i = 0;
+    double ** diagonalMatrix = createMat(n, n);
+    if (diagonalMatrix == NULL) {return NULL;}
+    zeroMat(diagonalMatrix, n, n);
+    for (; i < n; i++) {diagonalMatrix[i][i] = findTheDegreeOfaVertex(weightedAdjencyMatrix[i], n);}
+    return diagonalMatrix;
+}
+
+/*puts the Laplasian matrix in the diagonal matrix.*/
+void calcLaplasianMatrix(double ** weightedMatrix, double ** diagonalMatrix, int n) {
+    subtractMatrix(diagonalMatrix, weightedMatrix, n);
+}
